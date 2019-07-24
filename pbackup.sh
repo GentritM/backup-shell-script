98c3fc20e1787aea34312c94fe243df9eced9c82
GentritM <genti_mehmeti@outlook.com>
2019-07-24 16:56:18 +0200

    script added

---
+++ pbackup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+
+
+
+if [ $# -ne 2 ]; then 
+    echo "two args needed one for folder which you wanna back up and second for the destination folder"
+    exit 1 
+fi 
+
+
+target=$1
+#checking whether the target is a dir or a file only 
+if [ -f $target ]; then 
+    echo "target $target is a file it will be moved to $destination"
+else
+    
+    if [ -d $target ]; then 
+        echo "target $target is a directory it will be backed up to $destination"    
+    else 
+        echo "target is not valid, please enter a dir or a file"
+        exit 2 
+    fi
+fi 
+destination=$2 
+if [[ -f $destination ]]; then 
+    echo "destination must be a directory not a file"
+    exit 1 
+fi 
+
+date=$(date +%Y-%m-%d_%H_%M)
+mkdir -p $destination/$date 
+cp $target $destination/$date
+
+if [ $? -eq 0 ]; then 
+    echo backup ok 
+else 
+    echo backup failed 
+fi 
+
